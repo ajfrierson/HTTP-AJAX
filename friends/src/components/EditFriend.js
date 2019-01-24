7dbcea451fe0f56fe5180ae717981597b26b706d
ajfrierson <alvinfrierson@fuller.edu>
2019-01-23 21:52:47 -0800

    completed EditFriend.js

--- friends/src/components/EditFriend.js
+++ friends/src/components/EditFriend.js
@@ -9,3 +9,20 @@ const EditFriend = props => {
       onChange={props.editHandler} name="nameEdit" />
       <button onClick={props.saveEdit} value={props.nameValue}>Save</button>
       </form>
+
+      <form>
+      Age: <input placeholder="Age" type="number"
+      onChange={props.editHandler} name="ageEdit" />
+      <button onClick={props.saveEdit} value={props.ageValue}>Save</button>
+      </form>
+
+      <form>
+      Email: <input placeholder="Email" type="text"
+      onChange={props.editHandler} value={props.emailValue} name="emailEdit" />
+      <button onClick={props.saveEdit}>Save</button>
+      </form>
+    </div>
+  )
+}
+
+export default EditFriend;
